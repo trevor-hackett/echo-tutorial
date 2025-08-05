994ffcfd1ba983099239958eafe80e00f7bbf628
Trevor Hackett <1577822+trevor-hackett@users.noreply.github.com>
2025-08-04 21:19:47 -0600

    feat: add turborepi

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,13 +1,13 @@
-import { add } from "@workspace/math/add"
 import { Button } from "@workspace/ui/components/button"
+import { add } from "@workspace/math/add"
 
 export default function Page() {
 
-  const sum = add(2, 5)
+  const sum = add(8,3)
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello apps/web {sum}</h1>
+        <h1 className="text-2xl font-bold">Hello apps/widget {sum}</h1>
         <Button size="sm">Button</Button>
       </div>
     </div>
